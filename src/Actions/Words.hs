f1a43ae169e49df0f6c0c650fe253004f22b0802
Chris Penner <christopher.penner@gmail.com>
2018-11-19 16:02:21 -0600

    Fix bugs where we start multiple words at once

--- src/Actions/Words.hs
+++ src/Actions/Words.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Actions.Words where
 
 import Data.Words
@@ -7,35 +8,47 @@ import qualified Data.Text as T
 import Data.Stream.Infinite as S
 import Control.Monad.State
 import Control.Lens
+import Control.Applicative
+
+(<&&>) :: Applicative f => f Bool -> f Bool -> f Bool
+(<&&>) = liftA2 (&&)
+
+anyStarted :: (HasWords s, MonadState s m) => m Bool
+anyStarted = gets $ anyOf (eachWord . typed) (not . T.null)
 
 typeChar :: (HasWords s, HasWordStream s, MonadState s m) => Char -> m ()
 typeChar c = do
-  result <- gets (failover eachWord (tryType c))
-  case result of
-    Just newEnemies -> put newEnemies
-    Nothing         -> startWord c
+  anyStarted' <- anyStarted
+  if anyStarted' then typeExisting c else startNew c
 
-startWord :: (HasWords s, MonadState s m) => Char -> m ()
-startWord c = do
-  let doesMatch w | _typed w == T.empty =
-        T.singleton c `T.isPrefixOf` _untyped w
-  firstMatchIndex <- gets $ findOf (eachWord . withIndex) (doesMatch . snd)
+typeExisting :: (HasWords s, MonadState s m) => Char -> m ()
+typeExisting c = do
+  let isStarted w = w ^. typed /= ""
+  let doesMatch (view untyped -> T.uncons -> Just (c', _)) = c == c'
+      doesMatch _ = False
+  mMatchedWordIndex <-
+    preuse $ (eachWord . filtered (isStarted <&&> doesMatch) . asIndex)
+  case mMatchedWordIndex of
+    Nothing  -> return ()
+    Just ind -> eachWord . index ind %= typeCharInWord c
+
+startNew :: (HasWords s, MonadState s m) => Char -> m ()
+startNew c = do
+  let doesMatch w = T.singleton c `T.isPrefixOf` _untyped w
+  firstMatchIndex <- gets
+    $ findOf (eachWord . withIndex) ((doesMatch <&&> (T.null . _typed)) . snd)
   case firstMatchIndex of
     Nothing       -> pure ()
-    Just (ind, _) -> eachWord . index ind %= startTyping c
+    Just (ind, _) -> eachWord . index ind %= typeCharInWord c
 
-startTyping :: Char -> WordT -> WordT
-startTyping c w@(_untyped -> T.uncons -> Just (c', rest))
+typeCharInWord :: Char -> WordT -> WordT
+typeCharInWord c w@(_untyped -> T.uncons -> Just (c', _))
   | c == c'   = w & untyped %~ T.tail & typed %~ (|> c)
   | otherwise = w
+typeCharInWord _ w = w
 
 getWord :: (HasWordStream s, MonadState s m) => m T.Text
 getWord = do
   (a S.:> as) <- use wordStream
   wordStream .= as
   return a
-
-tryType :: Char -> WordT -> WordT
-tryType c w@(view untyped -> T.uncons -> Just (h, _rest)) | h == c =
-  ((w & typed %~ (|> h) & untyped %~ T.tail))
-tryType _ w = w
